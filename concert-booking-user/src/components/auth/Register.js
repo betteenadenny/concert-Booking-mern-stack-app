28f5e0a7f1167e7699b2a01d5509a85971e99569
Betteena Denny <betteenadenny@gmail.com>
2025-10-18 23:01:27 +0530

    updated register component

--- concert-booking-user/src/components/auth/Register.js
+++ concert-booking-user/src/components/auth/Register.js
@@ -5,14 +5,15 @@ import Navbar from "../Navbar";
 import CheckGuest from "./CheckGuest";
 
 function Register() {
-    var [name, setName] = useState('');
-    var [email, setEmail] = useState('');
-    var [password, setPassword] = useState('');
-    var [passwordConf, setPasswordConf] = useState('');
-    var [errorMessage, setErrorMessage] = useState('');
-    var navigate = useNavigate();
-    function registerUser(){
-        var user = {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConf, setPasswordConf] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
+    function registerUser(event){
+        event.preventDefault();
+        const user = {
             name: name,
             email: email,
             password: password,
@@ -22,91 +23,69 @@ function Register() {
             setErrorMessage('');
             navigate('/login');
         }).catch(error=>{
-            if(error.response.data.errors){
-                setErrorMessage(Object.values(error.response.data.errors).join(' '));
-            }else{
-                setErrorMessage('Failed to connect to api');
+            if (error.response) {
+                const data = error.response.data;
+                if (data.message) {
+                setErrorMessage(data.message);
+                } else if (data.errors) {
+                setErrorMessage(data.errors.join(' '));
+                } else {
+                setErrorMessage('Something went wrong. Please try again.');
+                }
+            } else {
+                setErrorMessage('Unable to connect to the server.');
             }
         })
     }
     return <div>
         <Navbar/>
- 
-<section class="text-center">
-
-    <div class="card mx-4 mx-md-5 shadow-5-strong bg-body-tertiary" style={{
-        marginTop: '10px',backdropFilter: 'blur(30px)',
-    }}>
-    <div class="card-body py-5 px-md-5">    
-        <div class="row d-flex justify-content-center">
-            <div class="col-lg-6">
-            <h2 class="fw-bold mb-5">Sign up now</h2>
-            <form>
-                <div data-mdb-input-init class="form-outline">
-                    <input type="text" id="form3Example1" class="form-control" />
-                    <label class="form-label" for="form3Example1">First name</label>
+        <div className="container d-flex align-items-center justify-content-center " style={{
+            marginTop: '10px',backdropFilter: 'blur(30px)'}}>
+            <div className="card-body py-5 px-md-5 ">    
+                <div className="row d-flex justify-content-center">
+                    <div className="col-6">
+                    <h2 className="fw-bold mb-5">Register now</h2>
+                    {errorMessage?<div className="alert alert-danger">{errorMessage}</div>:''}
+                    <form onSubmit={registerUser}>
+                        <div className="form-outline mb-2">
+                            <label htmlFor="name">Name</label>
+                            <input type="text"className="form-control" id="name" value={name} 
+                            onChange={(event)=>setName(event.target.value)}/>
+                        </div>
+                        <div className="form-group mb-2">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" className="form-control" id="email" value={email}
+                            onChange={(event)=>setEmail(event.target.value)}/>
+                        </div>
+                        <div className="form-group mb-2">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" className="form-control" id="password" value={password}
+                            onChange={(event)=>setPassword(event.target.value)} />
+                        </div>
+                        <div className="form-group mb-2">
+                            <label htmlFor="passwordConf">Confirm Password</label>
+                            <input type="password" className="form-control" id="passwordConf" value={passwordConf}
+                            onChange={(event)=>setPasswordConf(event.target.value)}/>
+                        </div>
+                        <div className="form-group ">
+                            <button type="submit" className="btn btn-primary btn-block mb-4">Register</button>
+                        </div>
+                    </form>
+                    </div>
                 </div>
-
-
-                <div data-mdb-input-init class="form-outline mb-4">
-                <input type="email" id="form3Example3" class="form-control" />
-                <label class="form-label" for="form3Example3">Email address</label>
-                </div>
-
-                <div data-mdb-input-init class="form-outline mb-4">
-                <input type="password" id="form3Example4" class="form-control" />
-                <label class="form-label" for="form3Example4">Password</label>
-                </div>
-
-                <div class="form-check d-flex justify-content-center mb-4">
-                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
-                <label class="form-check-label" for="form2Example33">
-                    Subscribe to our newsletter
-                </label>
-                </div>
-
-                <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">
-                Sign up
-                </button>
-
-                <div class="text-center">
-                <p>or sign up with:</p>
-                <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-facebook-f"></i>
-                </button>
-
-                <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-google"></i>
-                </button>
-
-                <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-twitter"></i>
-                </button>
-
-                <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-github"></i>
-                </button>
-                </div>
-            </form>
             </div>
         </div>
-        </div>
     </div>
-    </section>
+}
 
-        {/* <div className="container">
+export default CheckGuest(Register);
+
+
+{/* <div className="container">
             <div className="row">
                 <div className="col-8 offset-2">
-                    <h1>Register</h1>
+                    <h1>Login</h1>
                     {errorMessage?<div className="alert alert-danger">{errorMessage}</div>:''}
-                    <div className="form-group">
-                        <label>Name:</label>
-                        <input type="text"
-                        className="form-control"
-                        value={name}
-                        onInput={(event)=>setName(event.target.value)}
-                        />
-                    </div>
                     <div className="form-group">
                         <label>Email:</label>
                         <input type="text"
@@ -124,20 +103,8 @@ function Register() {
                         />
                     </div>
                     <div className="form-group">
-                        <label>Confirm Password:</label>
-                        <input type="password"
-                        className="form-control"
-                        value={passwordConf}
-                        onInput={(event)=>setPasswordConf(event.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary float-right" onClick={registerUser}>Submit</button>
+                        <button className="btn btn-primary float-right" onClick={attemptLogin}>Login</button>
                     </div>
                 </div>
             </div>
         </div> */}
-    </div>
-}
-
-export default CheckGuest(Register);
